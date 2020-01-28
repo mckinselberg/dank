ee142721bcf59068920c1d68c57506958ea56e04
Dan <dan@dankinsley.com>
2020-01-27 21:44:59 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <p>hi.</p>
+        <p>my name is dan kinsley. I used to play in a band called <a href="http://www.theunpronounceable.com">the unpronounceable</a>. Now, I'm a web developer and classical guitarist. </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        ></a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
