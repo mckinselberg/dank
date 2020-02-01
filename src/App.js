192adfd9770941b24e9d0a20827e6bb0a1784b62
Dan <dan@dankinsley.com>
2020-02-01 13:36:13 -0500

    removed animation and updated title
    removed animation and updated title

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './splash_logo.png';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <img src={logo} className="App-logo" alt="logo" />
         {/*
+        <img src={logo} className="App-logo" alt="logo" />
         <p>hi.</p>
         <p>my name is dan kinsley. I used to play in a band called <a href="http://www.theunpronounceable.com">the unpronounceable</a>. Now, I'm a web developer and classical guitarist. </p>
         <a
@@ -18,6 +20,7 @@ function App() {
           rel="noopener noreferrer"
         ></a>
         */}
+      <img src={logo} className="logo" alt="logo" />
       </header>
     </div>
   );
