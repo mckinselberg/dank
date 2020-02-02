c091cca5d2e7b959b0288c76667031754dbb0e8a
Dan <dan@dankinsley.com>
2020-02-01 22:38:14 -0500

    Update App.js
    clearing cruft

--- src/App.js
+++ src/App.js
@@ -10,21 +10,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        {/*
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>hi.</p>
-        <p>my name is dan kinsley. I used to play in a band called <a href="http://www.theunpronounceable.com">the unpronounceable</a>. Now, I'm a web developer and classical guitarist. </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        ></a>
-        <img src={logo} className="logo" alt="logo" />
-        <p>hmmm</p>
-        
-        */}
       <RandomAnimations />
       </header>
     </div>
