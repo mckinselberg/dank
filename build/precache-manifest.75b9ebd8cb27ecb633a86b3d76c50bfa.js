8aa898a221cdda0d8b1823ff49da692f7d91368b
Dan <dan@dankinsley.com>
2020-02-15 14:05:26 -0500

    re-adding mastermind
    commented out production build in .gitignore

---
+++ build/precache-manifest.75b9ebd8cb27ecb633a86b3d76c50bfa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8dbdbc6c87b10f0f2cb822a30b5b00c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51b63702e3e723909f3c",
+    "url": "/static/css/main.dec25648.chunk.css"
+  },
+  {
+    "revision": "ce89af482a9414af4a5c",
+    "url": "/static/js/2.27371570.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/static/js/2.27371570.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51b63702e3e723909f3c",
+    "url": "/static/js/main.971cc77e.chunk.js"
+  },
+  {
+    "revision": "f3d452f4e8a2b3f00d45",
+    "url": "/static/js/runtime-main.9f5a2457.js"
+  },
+  {
+    "revision": "d694dede6873a5ea9ae172f84cfe1c88",
+    "url": "/static/media/splash_logo.d694dede.png"
+  }
+]);
