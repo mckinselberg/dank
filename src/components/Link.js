89a63989641e64d620adac4ae2745d48e96c61f8
Dan <dan@dankinsley.com>
2020-02-27 11:51:08 -0500

    new resume. fixed error

--- src/components/Link.js
+++ src/components/Link.js
@@ -13,7 +13,7 @@ export default class Link extends Component {
 
     render() {
         return(
-            <li><a href={this.props.url} target="_blank">{this.props.name}</a></li>
+            <li><a href={this.props.url} target="_blank" rel="noopener noreferrer">{this.props.name}</a></li>
         );
     }
 }
