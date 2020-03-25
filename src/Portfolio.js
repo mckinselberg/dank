085279557da52fb7aa3ed6fa2641e1af7b3c891e
Dan <dan@dankinsley.com>
2020-03-25 10:51:23 -0400

    Updated AB link 1 with seg

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -89,7 +89,7 @@ export default class App extends Component{
         </div>
         <div className={`experience animated ${this.randomAnimation}`}>
           <ul>
-            <Link name="AllianceBernstein/a" url="https://www.alliancebernstein.com/sites/campaign/theme/tech-and-innovation.htm"/>
+            <Link name="AllianceBernstein/a" url="https://www.alliancebernstein.com/sites/campaign/theme/tech-and-innovation.htm?Seg=56"/>
             <Link name="AllianceBernstein/b" url="https://www.alliancebernstein.com/investments/us/retirement/collective-investment-trusts/home.htm"/>
             <Link name="AllianceBernstein/c" url="https://www.alliancebernstein.com/investments/us/retirement/lifetime-income-strategy/home.htm"/>
             {/*<Link name="AllianceBernstein/d" url="https://www.alliancebernstein.com/investments/us/retirement/ira/home.htm"/>*/}
