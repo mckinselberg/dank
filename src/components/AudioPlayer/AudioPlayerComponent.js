232f89bc5d6ab2fd76720564b18a69a8e857df9f
Dan <dan@dankinsley.com>
2020-12-19 17:56:54 -0500

    Continued AudioPlayerComponent development

--- src/components/AudioPlayer/AudioPlayerComponent.js
+++ src/components/AudioPlayer/AudioPlayerComponent.js
@@ -1,157 +1,140 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, Component } from 'react';
 import '../../css/audio-player.css';
 //import ReactDOM from 'react-dom';
 
-// initialize audio context outside of function component scope
+
 const AudioContext = window.AudioContext || window.webkitAudioContext; // <-- for legacy browsers
 // instantiate audio context in app
-const audioContext = new AudioContext();
-// create audio element on the fly or use document.querySelector('audio');
-let audioElement = document.createElement('audio');
-audioElement.crossOrigin = "anonymous";
-audioElement.src = "/music/boombap.mp3";
+//const audioContext = new AudioContext();
 
-if (audioContext.state === 'suspended') {
-  audioContext.resume();
-}
 
-const songs = [
-  {
-    name: 'reset',
-    url: 'fx/reset.mp3'
-  },
-  {
-    name: 'saturday',
-    url: '201213 Saturday.mp3'
-  },
-  {
-    name: 'run',
-    url: 'experimental/run.mp3'
-  },
-  {
-    name: 'crazy drums',
-    url: 'experimental/crazy-drums.mp3',
-    album: '',    
-  },
-  {
-    name: 'more',
-    url: 'more.mp3'
-  },
-  {
-    name: 'more extended',
-    url: 'more-extended.mp3'
-  },
-  {
-    name: "drive",
-    url:"unpro/drive-004.mp3"
-  },
-  {
-    name: "boom bap",
-    url:"boombap.mp3"
-  },
-  {
-    name: "boom bap nope",
-    url:"boombapnope.mp3"
-  },
-  {
-    name: 'glory box',
-    url: 'Glory Box.mp3'
-  },
-  {
-    name: 'noir',
-    url: 'experimental/noir.mp3'
-  },
-  {
-    name: 'noir too',
-    url: 'experimental/noir2.mp3'
-  },
-  {
-    name: 'noir 3',
-    url: 'experimental/170206-noir-3.mp3'
-  },
-  {
-    name: 'quarantine',
-    url: 'Quarantine.mp3'
-  },
-  {
-    name: 'yamahahaha',
-    url: '181102-001.mp3'
+export default class AudioPlayer extends Component{
+  constructor() {
+    super();
+    this.songs = [
+      {
+        name: 'reset',
+        url: 'fx/reset.mp3'
+      },
+      {
+        name: 'saturday',
+        url: '201213 Saturday.mp3'
+      },
+      {
+        name: 'run',
+        url: 'experimental/run.mp3'
+      },
+      {
+        name: 'crazy drums',
+        url: 'experimental/crazy-drums.mp3',
+        album: '',    
+      },
+      {
+        name: 'more',
+        url: 'more.mp3'
+      },
+      {
+        name: 'more extended',
+        url: 'more-extended.mp3'
+      },
+      {
+        name: "drive",
+        url:"unpro/drive-004.mp3"
+      },
+      {
+        name: "boom bap",
+        url:"boombap.mp3"
+      },
+      {
+        name: "boom bap nope",
+        url:"boombapnope.mp3"
+      },
+      {
+        name: 'glory box',
+        url: 'Glory Box.mp3'
+      },
+      {
+        name: 'noir',
+        url: 'experimental/noir.mp3'
+      },
+      {
+        name: 'noir too',
+        url: 'experimental/noir2.mp3'
+      },
+      {
+        name: 'noir 3',
+        url: 'experimental/170206-noir-3.mp3'
+      },
+      {
+        name: 'quarantine',
+        url: 'Quarantine.mp3'
+      },
+      {
+        name: 'yamahahaha',
+        url: '181102-001.mp3'
+      }
+    ];
+    this.state = {
+      playState: 'is paused',
+      song: this.songs[0]
+    }
+    this.musicFolder = 'music/'
+    this.audioElement = new Audio(`${this.musicFolder}boombap.mp3`);
+    this.audioElement.crossOrigin = 'anonymous';
   }
-];
-
-export default function AudioPlayerComponent() {
-  const [playState, setPlayState] = useState('is paused');
-  const [currentSong, setSong] = useState("boombap");
-  const refAudioEl = useRef(audioElement);
-  const [audioElState, setAudioElState] = useState(refAudioEl.current);
-  const refAudioContextState = useRef(audioContext);
-  const [audioContextState, setAudioContextState] = useState(refAudioContextState)
   
-
-  function pressPlay() {   
-    // control audio element 
-    if (audioElement.paused) {
-      audioElement.play();
+  pressPlay = () => {
+    this.audioElement.play();
+    if (this.audioElement.paused) {
+      this.audioElement.play();
     } else {
-      audioElement.pause();
+      this.audioElement.pause();
     }
     // control ui
-    if (playState === 'is paused' || playState === 'has ended') {
-      setPlayState('is playing');
+    if (this.state.playState === 'is paused' || this.state.playState === 'has ended') {
+      this.setState({playState:'is playing'});
     } else {
-      setPlayState('is paused');
+      this.setState({playState:'is paused'});
     }
-    setAudioContextState(refAudioContextState);
+    //debugger;
   }
 
-  function changeSong(song, url) {
-    audioElement.src = `/music/${url}`;
-    setSong(song);
-    setAudioElState(refAudioEl.current);
-    setAudioContextState(refAudioContextState.current);
-    setPlayState('is playing');
-    audioElement.play();
+  changeSong = (song, url) => {
+    console.log('change song pressed');
+    this.audioElement.src = `/music/${url}`;
+    //setSongDuration(audioElement.duration);
+    this.setState({
+      playState:'is playing',
+      song:song
+    });
+    //setAudioElState(refAudioEl.current);
+    //setAudioContextState(refAudioContext.current);
+    //setPlayState('is playing');
+    this.audioElement.play();
   }
 
-  // gain node
-  const gainNode = audioContext.createGain();
-  // gainNode.gain.value = 0;
-  // connect gain node to audioContext.destination
-  // track.connect(gainNode).connect(audioContext.destination);
-
-  const volumeControl = document.createElement('input');
-  volumeControl.type="range";
-  volumeControl.id="volume";
-  volumeControl.min="0";
-  volumeControl.max="2";
-  volumeControl.vsalue="1";
-  volumeControl.step="0.01";
-
-  volumeControl.addEventListener('input', function() {
-    gainNode.gain.value = this.value;
-  }, false);
-
-  return (
+  render() {
+    return (
     <>
-      {(()=>{
-      })()}
-
-      {refAudioEl.current.addEventListener('ended', (e) => {
-        setPlayState('has ended');
-      })}
+      {console.log(this.audioElement)}
       
-      <button className="play-button" onClick={pressPlay}>{playState === 'is paused' || playState === 'has ended' ? 'Play' : 'Pause'}</button>
+      <button className="play-button" onClick={this.pressPlay}>{
+        this.state.playState === 'is paused' || 
+        this.state.playState === 'has ended' ? 
+        'Play' : 'Pause'}</button>
       <br/>
-      <h1 className={`current-song`}>{`"${currentSong}" ${playState}`}</h1>
-      <br/>
-      <br/>
+      <h1 className={`current-song`}>{`"${this.state.song.name}" ${this.state.playState}`}</h1>
+      {/* <p style={{textAlign:'center'}}>{reskinTime(songCurrentTime)} of {reskinTime(songDuration)}</p> */}
       <div className="songs">
-        {songs.map((song,i) => {
+        {this.songs.map((song,i) => {
           return (
-            <button key={i} onClick={()=>{changeSong(song.name, song.url)}}>{song.name}</button>
+            <button key={i} onClick={()=>{this.changeSong(this.songs[i])}}>{song.name}</button>
           );
         })}
       </div>
     </>
-  )
+    );
+  }
+
 }
+
---
+++ src/components/AudioPlayer/AudioPlayerComponent.v2.js
@@ -0,0 +1,185 @@
+import React, { useState, useRef } from 'react';
+import '../../css/audio-player.css';
+//import ReactDOM from 'react-dom';
+
+// initialize audio context outside of function component scope
+const AudioContext = window.AudioContext || window.webkitAudioContext; // <-- for legacy browsers
+// instantiate audio context in app
+const audioContext = new AudioContext();
+// create audio element on the fly or use document.querySelector('audio');
+let audioElement = document.createElement('audio');
+audioElement.crossOrigin = "anonymous";
+audioElement.src = "/music/boombap.mp3";
+
+if (audioContext.state === 'suspended') {
+  audioContext.resume();
+}
+
+const songs = [
+  {
+    name: 'reset',
+    url: 'fx/reset.mp3'
+  },
+  {
+    name: 'saturday',
+    url: '201213 Saturday.mp3'
+  },
+  {
+    name: 'run',
+    url: 'experimental/run.mp3'
+  },
+  {
+    name: 'crazy drums',
+    url: 'experimental/crazy-drums.mp3',
+    album: '',    
+  },
+  {
+    name: 'more',
+    url: 'more.mp3'
+  },
+  {
+    name: 'more extended',
+    url: 'more-extended.mp3'
+  },
+  {
+    name: "drive",
+    url:"unpro/drive-004.mp3"
+  },
+  {
+    name: "boom bap",
+    url:"boombap.mp3"
+  },
+  {
+    name: "boom bap nope",
+    url:"boombapnope.mp3"
+  },
+  {
+    name: 'glory box',
+    url: 'Glory Box.mp3'
+  },
+  {
+    name: 'noir',
+    url: 'experimental/noir.mp3'
+  },
+  {
+    name: 'noir too',
+    url: 'experimental/noir2.mp3'
+  },
+  {
+    name: 'noir 3',
+    url: 'experimental/170206-noir-3.mp3'
+  },
+  {
+    name: 'quarantine',
+    url: 'Quarantine.mp3'
+  },
+  {
+    name: 'yamahahaha',
+    url: '181102-001.mp3'
+  }
+];
+
+export default function AudioPlayerComponent() {
+  const [playState, setPlayState] = useState('is paused');
+  const [currentSong, setSong] = useState("boombap");
+  const [songDuration, setSongDuration] = useState(null);
+  const [songCurrentTime, setSongCurrentTime] = useState(null);
+  const refAudioElement = useRef(audioElement);
+  //const [audioElState, setAudioElState] = useState(refAudioEl.current);
+  //const refAudioContext = useRef(audioContext);
+  //const [audioContextState, setAudioContextState] = useState(refAudioContext)
+  
+
+  function pressPlay() {
+    // control audio element 
+    if (audioElement.paused) {
+      audioElement.play();
+    } else {
+      audioElement.pause();
+    }
+    // control ui
+    if (playState === 'is paused' || playState === 'has ended') {
+      setPlayState('is playing');
+    } else {
+      setPlayState('is paused');
+    }
+    //setAudioContextState(refAudioContext);
+  }
+
+  function changeSong(song, url) {
+    audioElement.src = `/music/${url}`;
+    setSongDuration(audioElement.duration);
+    setSong(song);
+    //setAudioElState(refAudioEl.current);
+    //setAudioContextState(refAudioContext.current);
+    setPlayState('is playing');
+    audioElement.play();
+  }
+
+  function reskinTime(secs, format) {
+    var hr = Math.floor(secs / 3600);
+    var min = Math.floor((secs - (hr * 3600)) / 60);
+    var sec = Math.floor(secs - (hr * 3600) - (min * 60));
+    if (sec < 10) {
+      sec = "0" + sec;
+    }
+    return min + ':' + sec;
+  }
+
+  // gain node
+  const gainNode = audioContext.createGain();
+  // gainNode.gain.value = 0;
+  // connect gain node to audioContext.destination
+  // track.connect(gainNode).connect(audioContext.destination);
+
+  const volumeControl = document.createElement('input');
+  volumeControl.type="range";
+  volumeControl.id="volume";
+  volumeControl.min="0";
+  volumeControl.max="2";
+  volumeControl.vsalue="1";
+  volumeControl.step="0.01";
+
+  volumeControl.addEventListener('input', function() {
+    gainNode.gain.value = this.value;
+  }, false);
+
+  return (
+    <>
+      {(()=>{
+        //console.clear();
+        //console.log(refAudioEl.current);
+        console.log(audioElement.currentTime);
+        //console.log(songDuration);
+        //console.log(refAudioContext.current)
+      })()}
+
+      {refAudioElement.current.addEventListener('loadedmetadata', () => {
+        setSongDuration(refAudioElement.current.duration);
+      })}
+
+      {refAudioElement.current.addEventListener('timeupdate', () => {
+        //setSongCurrentTime(refAudioElement.current.currentTime);
+      })}
+
+      {refAudioElement.current.addEventListener('ended', (e) => {
+        setPlayState('has ended');
+      })}
+      
+      <button className="play-button" onClick={pressPlay}>{playState === 'is paused' || playState === 'has ended' ? 'Play' : 'Pause'}</button>
+      <br/>
+      <h1 className={`current-song`}>{`"${currentSong}" ${playState}`}</h1>
+      <p style={{textAlign:'center'}}>{reskinTime(songCurrentTime)} of {reskinTime(songDuration)}</p>
+      <p style={{textAlign:'center'}}></p>
+      <br/>
+      <br/>
+      <div className="songs">
+        {songs.map((song,i) => {
+          return (
+            <button key={i} onClick={()=>{changeSong(song.name, song.url)}}>{song.name}</button>
+          );
+        })}
+      </div>
+    </>
+  )
+}
