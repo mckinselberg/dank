1a7c73f4e9b6ad3701bb7275f5475ac06c8a6a1c
Dan <dan@dankinsley.com>
2022-04-20 11:42:08 -0400

    Update teknkl-simpledto-1.0.4.js
    added ie polyfills. renamed variables to make flow easier to follow.

--- public/teknkl-simpledto-1.0.4.js
+++ public/teknkl-simpledto-1.0.4.js
@@ -1,54 +1,97 @@
 /*
- @author Sanford Whiteman, TEKNKL (blog.teknkl.com / sandy@teknkl.com)
+ @author Sanford Whiteman, TEKNKL (blog.teknkl.com / sandy@teknkl.com) with Revisions/Refinements by Dan Kinsley
  @version v1.0.4
  @copyright Copyright 2016, 2017, 2018, 2019 FigureOne, Inc.
  @license MIT License: You must include this license and the above credits in all uses & reproductions of this software.
 */
-function SimpleDTO(a) {
-  var e = Function.prototype.call.bind(Array.prototype.slice);
+
+
+Array.prototype.forEach =
+  Array.prototype.forEach ||
+  function (fn, scope) {
+    "use strict";
+    var i, len;
+    for (i = 0, len = this.length; i < len; ++i) {
+      if (i in this) {
+        fn.call(scope, this[i], i, this);
+      }
+    }
+  };
+Array.prototype.find =
+  Array.prototype.find ||
+  function (callback) {
+    if (this === null) {
+      throw new TypeError(
+        "Array.prototype.find called on null or undefined"
+      );
+    } else if (typeof callback !== "function") {
+      throw new TypeError("callback must be a function");
+    }
+    var list = Object(this);
+    // Makes sures is always has an positive integer as length.
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    for (var i = 0; i < length; i++) {
+      var element = list[i];
+      if (callback.call(thisArg, element, i, list)) {
+        return element;
+      }
+    }
+  };
+
+function SimpleDTO(initObj) {
+  var slice = Function.prototype.call.bind(Array.prototype.slice);
   try {
-    a.debug && console.log("SimpleDTO: Unifying domains " + [document.domain, a.domain].join(", ")), document.domain = a.domain;
+    initObj.debug && console.log("SimpleDTO: Unifying domains " + [document.domain, initObj.domain].join(", ")), document.domain = initObj.domain;
   } catch (b) {
-    throw "SimpleDTO: Domain unification error, domain: " + a.domain;
+    throw "SimpleDTO: Domain unification error, domain: " + initObj.domain;
   }
-  if ("receive" == a.mode) {
-    var c = document.createElement("iframe");
+  if ("receive" == initObj.mode) {
+    var iframe = document.createElement("iframe");
     this.setSource = function(b) {
-      c.src = b;
+      iframe.src = b;
     };
-    this.getSource = function() {
-      return d;
-    };
-    c.addEventListener("load", function() {
-      this.data || this.src ? (a.debug && console.log("SimpleDTO: running callback"), a.cb && a.cb.call(this, f)) : console.warn("SimpleDTO: skipping load event due to empty data src or callback");
+    // this.getSource = function() {
+    //   return d;
+    // };
+    iframe.addEventListener("load", function() {
+      this.data || this.src ? (initObj.debug && console.log("SimpleDTO: running callback"), initObj.cb && initObj.cb.call(this, f)) : console.warn("SimpleDTO: skipping load event due to empty data src or callback");
     });
-    c.setAttribute("data-transfer-object", "true");
+    iframe.setAttribute("data-transfer-object", "true");
     [["visibility", "hidden"], ["position", "absolute"], ["height", "0"]].forEach(function(b) {
-      c.style.setProperty.apply(c.style, b);
+      iframe.style.setProperty.apply(iframe.style, b);
     });
-    var d = document.createElement("a");
-    d.href = a.dataSrc || "";
-    a.noReplaceQuery || (d.search = document.location.search);
-    window.__mktTokVal && (d.search += (1 < d.search.length ? "&" : "") + ["mkt_tok", window.__mktTokVal].join("="));
-    a.noInit || this.setSource(d.href);
-    document.body.appendChild(c);
+    var anchor = document.createElement("a");
+    anchor.href = initObj.dataSrc || "";
+    initObj.noReplaceQuery || (anchor.search = document.location.search);
+    window.__mktTokVal && (anchor.search += (1 < anchor.search.length ? "&" : "") + ["mkt_tok", window.__mktTokVal].join("="));
+    initObj.noInit || this.setSource(anchor.href);
+    document.body.appendChild(iframe);
   }
   var f = this;
-  return {getGlobal:function() {
-    return c.contentWindow;
-  }, cleanup:function() {
-    var b = c.contentWindow.frameElement;
-    b.parentNode.removeChild(b);
-  }, parse:function(b) {
-    b = document.querySelector('.dto-xml[data-field-collection="' + b + '"]').text;
-    b = (new DOMParser).parseFromString(b, "application/xml");
-    var a = b.querySelector("mktoPreFillFields");
-    a = a.getAttribute("varName") || a.tagName;
-    var c = {};
-    e(b.querySelectorAll("mktoPreFillFields mktoField")).forEach(function(a) {
-      c[a.getAttribute("inputName")] = a.textContent;
-    });
-    return self[a] = c;
-  }};
+  return {
+    getGlobal:function() {
+      return iframe.contentWindow;
+    }, 
+    cleanup:function() {
+      var b = iframe.contentWindow.frameElement;
+      console.log(b)
+      b.parentNode.removeChild(b);
+    }, 
+    parse:function(dataFieldCollectionName) {
+      var dataFieldCollection = document.querySelector('.dto-xml[data-field-collection="' + dataFieldCollectionName + '"]').text;
+      var parsedXML = (new DOMParser).parseFromString(dataFieldCollection, "application/xml");
+      var mktoPreFillFields = parsedXML.querySelector("mktoPreFillFields");
+      var tagName = mktoPreFillFields.getAttribute("varName") || mktoPreFillFields.tagName;
+      var c = {};
+      // console.log(mktoPreFillFields.querySelectorAll("mktoPreFillFields mktoField"));
+      slice(parsedXML.querySelectorAll("mktoPreFillFields mktoField")).forEach(function(mktoField,i) {
+        // console.log(i + ": ", mktoField);
+        c[mktoField.getAttribute("inputName")] = mktoField.textContent;
+      });
+      return self[tagName] = c;
+    },
+    slice: slice
+  };
 }
 window.SimpleDTO = SimpleDTO;
